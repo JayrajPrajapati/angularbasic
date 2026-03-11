64a9998b19057065803c96aa86b766717cd31d41
Jayraj Chhaya <jayrajchhaya@gmail.com>
2026-03-11 15:07:05 +0530

    Learned project folder structure and implemented string interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,22 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  //template: `<h1>Inline Template with InterPolation {{Tech}}</h1>`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angularbasic');
+  protected readonly title = signal('Angular Basic');
+  angularVersion ='V21.0.0.0';
+  
+  Tech = 'Angular';
+  Version =21.0;
+
+  getTechWithVersion()
+  {
+    return "Tech is : <b>" + this.Tech +"</b> & Version is : <b>" + this.Version +"</b>"
+  }
+  GetTotal(a:number,b:number)
+  {
+    return a+b;
+  }
 }
