e523cfb7f7371ebea56b8872019774a159e289c3
Jayraj Chhaya <jayrajchhaya@gmail.com>
2026-03-11 19:07:09 +0530

    Event binding with params

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,6 +16,7 @@ export class App {
   Tech = 'Angular';
   Version =21.0;
 
+  count = 0;
   getTechWithVersion()
   {
     return "Tech is : <b>" + this.Tech +"</b> & Version is : <b>" + this.Version +"</b>"
@@ -28,4 +29,31 @@ export class App {
   {
     alert('Button Event Bind...');
   }
+  counter(action:string)
+  {
+    if(action === '-' )
+      this.count>0  && this.count--
+    else
+      this.count++
+  }
+  handleEvnet(eventType:string)
+  {
+    alert('Event Type is :'+eventType);
+  }
+  data = 10;
+  updateData(val:number,user:string)
+  {
+this.data = val;
+console.log(user);
+console.log(this.sum(10,20));
+  }
+  sum(a:number,b:number):number
+  {
+    return a+b;
+  }
+  handleButtonEvnet(event:any)
+{
+console.log(event);
+
+}
 }
