d5bd539e1fac40ae793056d12be114ca0f137c15
Jayraj Chhaya <jayrajchhaya@gmail.com>
2026-03-13 13:03:16 +0530

    Implment the basic Signal and understood the concept

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,64 +1,156 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignUp } from './componenets/sign-up/sign-up';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SignUp],
+  imports: [RouterOutlet, SignUp],
   templateUrl: './app.html',
-  //template: `<h1>Inline Template with InterPolation {{Tech}}</h1>`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Angular Basic');
-  angularVersion ='V21.0.0.0';
-  
-  Tech = 'Angular';
-  Version =21.0;
 
-  count = 0;
-  getTechWithVersion()
-  {
-    return "Tech is : <b>" + this.Tech +"</b> & Version is : <b>" + this.Version +"</b>"
+  /*-----------------------------------------
+    BASIC PROJECT INFORMATION
+  -----------------------------------------*/
+
+  // Angular signal example
+  protected readonly title = signal('Angular Basic');
+
+  // Angular version variable
+  angularVersion = 'V21.0.0.0';
+
+  // Technology and version
+  Tech = 'Angular';
+  Version = 21.0;
+
+
+  /*-----------------------------------------
+    STRING INTERPOLATION EXAMPLE
+  -----------------------------------------*/
+
+  // Returns HTML string with tech and version
+  getTechWithVersion() {
+    return "Tech is : <b>" + this.Tech + "</b> & Version is : <b>" + this.Version + "</b>";
   }
-  GetTotal(a:number,b:number)
-  {
-    return a+b;
+
+
+  /*-----------------------------------------
+    FUNCTION BINDING EXAMPLE
+  -----------------------------------------*/
+
+  // Simple function to calculate total
+  GetTotal(a: number, b: number) {
+    return a + b;
   }
-  callButtonEvent()
-  {
+
+
+  /*-----------------------------------------
+    EVENT BINDING EXAMPLE
+  -----------------------------------------*/
+
+  // Simple button click event
+  callButtonEvent() {
     alert('Button Event Bind...');
   }
-  counter(action:string)
-  {
-    if(action === '-' )
-      this.count>0  && this.count--
-    else
-      this.count++
-  }
-  handleEvnet(eventType:string)
-  {
-    alert('Event Type is :'+eventType);
-  }
-  data = 10;
-  updateData(val:number,user:string)
-  {
-this.data = val;
-console.log(user);
-console.log(this.sum(10,20));
-  }
-  sum(a:number,b:number):number
-  {
-    return a+b;
-  }
-  handleButtonEvnet(event:any)
-{
-console.log(event);
 
-}
-//Property Binding
-btnDisableDynamic=true;
-btnToggle(){
-  this.btnDisableDynamic =! this.btnDisableDynamic;
-}
+  // Counter example with + and -
+  count = 0;
+
+  counter(action: string) {
+    if (action === '-') {
+      this.count > 0 && this.count--;
+    } else {
+      this.count++;
+    }
+  }
+
+  // Generic event handler
+  handleEvnet(eventType: string) {
+    alert('Event Type is : ' + eventType);
+  }
+
+
+  /*-----------------------------------------
+    FUNCTION WITH PARAMETERS
+  -----------------------------------------*/
+
+  data = 10;
+
+  updateData(val: number, user: string) {
+    this.data = val;
+
+    console.log("User:", user);
+    console.log("Sum Result:", this.sum(10, 20));
+  }
+
+  // Utility function
+  sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  // Capture full event object
+  handleButtonEvnet(event: any) {
+    console.log(event);
+  }
+
+
+  /*-----------------------------------------
+    PROPERTY BINDING EXAMPLE
+  -----------------------------------------*/
+
+  // Used to dynamically disable button
+  btnDisableDynamic = true;
+
+  // Toggle button disabled state
+  btnToggle() {
+    this.btnDisableDynamic = !this.btnDisableDynamic;
+  }
+
+
+  /*-----------------------------------------
+    SIGNAL VS NORMAL PROPERTY
+  -----------------------------------------*/
+
+  // Simple variable
+  simplePropertyData = 0;
+
+  // Angular Signal
+  signalPropertyData = signal(0);
+
+
+  /*-----------------------------------------
+    SIGNAL EFFECT
+    Runs automatically when signal changes
+  -----------------------------------------*/
+
+  constructor() {
+    effect(() => {
+
+      console.log("Simple Property", this.simplePropertyData);
+
+      console.log("Signal Property", this.signalPropertyData());
+
+      // Reset signal when value reaches 5
+      if (this.signalPropertyData() === 5) {
+        this.signalPropertyData.set(0);
+        console.log("Signal Property Reset", this.signalPropertyData());
+      }
+
+    });
+  }
+
+
+  /*-----------------------------------------
+    UPDATE METHODS
+  -----------------------------------------*/
+
+  updateSimplePropertyData() {
+    this.simplePropertyData++;
+  }
+
+  updateSinglePropertyData() {
+    this.signalPropertyData.set(this.signalPropertyData() + 1);
+  }
+
 }
