3765045feebba700f9dfd7458eeb595e84a253bf
Jayraj Chhaya <jayrajchhaya@gmail.com>
2026-03-17 16:19:58 +0530

    Created a to-do list with CRUD

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, computed, effect, signal, WritableSignal } from '@angular/co
 import { RouterOutlet } from '@angular/router';
 import { SignUp } from './componenets/sign-up/sign-up';
 import { FormsModule } from '@angular/forms';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignUp,FormsModule],
+  imports: [RouterOutlet, SignUp, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -157,20 +158,18 @@ export class App {
   //Computed Signal
   height = signal(100);
   width = signal(20);
-  area = computed(()=> this.height()*this.width())
-  
-  handleHeight()
-  {
-    this.height.set(this.height()+10);
+  area = computed(() => this.height() * this.width())
+
+  handleHeight() {
+    this.height.set(this.height() + 10);
   }
   //Effect Signal
   speed = signal(0);
   color = "blue";
-  increaseSpeed()
-  {
-    this.speed.set(this.speed()+10);
+  increaseSpeed() {
+    this.speed.set(this.speed() + 10);
   }
-   constructor() {
+  constructor() {
     effect(() => {
       console.log("Simple Property", this.simplePropertyData);
       console.log("Signal Property", this.signalPropertyData());
@@ -182,104 +181,114 @@ export class App {
       }
 
       //Effect Signal for Speed
-      if (this.speed()>0 && this.speed()<40) {
-        this.color='green';
+      if (this.speed() > 0 && this.speed() < 40) {
+        this.color = 'green';
       }
-      if (this.speed()>=40 && this.speed()<80) {
-        this.color='blue';
+      if (this.speed() >= 40 && this.speed() < 80) {
+        this.color = 'blue';
       }
-      if (this.speed()>=80 && this.speed()<120) {
-        this.color='orange';
+      if (this.speed() >= 80 && this.speed() < 120) {
+        this.color = 'orange';
       }
-      if (this.speed()>=120) {
-        this.color='red';
+      if (this.speed() >= 120) {
+        this.color = 'red';
       }
       console.log("Speed Increased", this.speed());
     });
   }
 
   //Data Types in Signal
-  user:WritableSignal<string[]> = signal(['Angular','React Js','React Native'])
-  getUpdatedUserData()
-  {
-    console.log("Old Value "+this.user());
-    this.user.update((item)=>[...item,'Vue Js'])
+  user: WritableSignal<string[]> = signal(['Angular', 'React Js', 'React Native'])
+  getUpdatedUserData() {
+    console.log("Old Value " + this.user());
+    this.user.update((item) => [...item, 'Vue Js'])
     console.log("Updated value" + this.user());
-    
+
   }
   //Counter in Signal
-  counterApp:WritableSignal<number>=signal<number>(0);
-  incrementCounter()
-  {
-    this.counterApp.update((val)=>val+1);
+  counterApp: WritableSignal<number> = signal<number>(0);
+  incrementCounter() {
+    this.counterApp.update((val) => val + 1);
   }
-  resetCounter()
-  {
+  resetCounter() {
     this.counterApp.set(0);
   }
-  decrementCounter()
-  {
-    if (this.counterApp()>0) {
-      this.counterApp.update((val)=>val-1);
+  decrementCounter() {
+    if (this.counterApp() > 0) {
+      this.counterApp.update((val) => val - 1);
     }
   }
   //Get & Set Input value via Signals
-  inputValue :WritableSignal<string>= signal<string>("Intialization");
-  setResetInputValue()
-  {
+  inputValue: WritableSignal<string> = signal<string>("Intialization");
+  setResetInputValue() {
     this.inputValue.set("I'm Default Input Value");
   }
-  setInputValue(val:string)
-  {
+  setInputValue(val: string) {
     this.inputValue.set(val)
   }
   //Control Flow Statement
   isLogIn = signal(false);
   show = signal(true);
-  handleLogin(status:boolean)
-  {
+  handleLogin(status: boolean) {
     this.isLogIn.set(status)
   }
-   //Control Flow  for For loop Statement
- languages:WritableSignal<string[]> = signal(['Angular','React Js','React Native','Vue JS','Node JS'])
- languageDetail = signal([
-  {id: 1, name: 'Angular',developedBy: 'Google',latestVersion: 21},
-  {id: 2,name: 'React JS',developedBy: 'Meta (Facebook)',latestVersion: 19},
-  {id: 3,name: 'React Native',developedBy: 'Meta (Facebook)',latestVersion: 0.74},
-  {id: 4,name: 'Vue JS',developedBy: 'Evan You',latestVersion: 3},
-  {id: 5,name: 'Node JS',developedBy: 'Ryan Dahl',latestVersion: 22}
-]);
+  //Control Flow  for For loop Statement
+  languages: WritableSignal<string[]> = signal(['Angular', 'React Js', 'React Native', 'Vue JS', 'Node JS'])
+  languageDetail = signal([
+    { id: 1, name: 'Angular', developedBy: 'Google', latestVersion: 21 },
+    { id: 2, name: 'React JS', developedBy: 'Meta (Facebook)', latestVersion: 19 },
+    { id: 3, name: 'React Native', developedBy: 'Meta (Facebook)', latestVersion: 0.74 },
+    { id: 4, name: 'Vue JS', developedBy: 'Evan You', latestVersion: 3 },
+    { id: 5, name: 'Node JS', developedBy: 'Ryan Dahl', latestVersion: 22 }
+  ]);
 
-//Control Flow for Switch Statement
-switchStatus = signal("");
-handleSwitch(event:Event)
-{
-  const target = event.target as HTMLSelectElement;
-  this.switchStatus.set(target.value);
-}
-//TWO-WAY DATA BINDING EXAMPLES
+  //Control Flow for Switch Statement
+  switchStatus = signal("");
+  handleSwitch(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.switchStatus.set(target.value);
+  }
+  //TWO-WAY DATA BINDING EXAMPLES
 
-// Signal-based binding (manual approach)
-withoutNgModel = signal('Without NgModel (using Signal)');
-// Simple property binding using NgModel
-withNgModel: string = 'With NgModel (without Signal)';
-userObj = signal({
-  name:'Developer',
-  experience :5,
-  contactNo:'7778523610'
-});
-updateUserObj(key:string,val:string){
-  this.userObj.update((item)=>({...item,[key]:val}))
-}
+  // Signal-based binding (manual approach)
+  withoutNgModel = signal('Without NgModel (using Signal)');
+  // Simple property binding using NgModel
+  withNgModel: string = 'With NgModel (without Signal)';
+  userObj = signal({
+    name: 'Developer',
+    experience: 5,
+    contactNo: '7778523610'
+  });
+  updateUserObj(key: string, val: string) {
+    this.userObj.update((item) => ({ ...item, [key]: val }))
+  }
 
-//Get & Set Method for Signal | Two way Binding
-getterSetter = signal('Getter & Setter Method for Two way Binding');
-get getterSetterVal()
-{
-  return this.getterSetter();
-}
-set getterSetterVal(val:string)
-{
-  this.getterSetter.set(val);
-}
+  //Get & Set Method for Signal | Two way Binding
+  getterSetter = signal('Getter & Setter Method for Two way Binding');
+  get getterSetterVal() {
+    return this.getterSetter();
+  }
+  set getterSetterVal(val: string) {
+    this.getterSetter.set(val);
+  }
+  //ToDo List with Singnals
+  tasks = signal([
+  { id: 1, title: "Interview", completed: false },
+  { id: 2, title: "Coding Practice", completed: false },
+  { id: 3, title: "Client Meeting", completed: false },
+  { id: 4, title: "Project Deployment", completed: false },
+  { id: 5, title: "Learn Angular Signals", completed: false }
+])
+  taskTitle = signal('');
+  addTask() {
+    if (this.taskTitle()) {
+      const nextId = this.tasks().length ? Math.max(...this.tasks().map(t => t.id)) + 1 : 1;
+      this.tasks.update((item) => (
+        [...item, { id:nextId, title: this.taskTitle(), completed: false }]));
+      this.taskTitle.set('');
+    }
+  }
+  deleteTask(id: number) {
+    this.tasks.update((tasks) => tasks.filter((task) => task.id != id))
+  }
 }
