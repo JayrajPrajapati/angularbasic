b7d8bb3d7208da5a981b237a4515116100339e3d
Jayraj Chhaya <jayrajchhaya@gmail.com>
2026-03-23 12:06:43 +0530

    Implmented a dynamic component loading

--- src/app/componenets/pages/about/about.ts
+++ src/app/componenets/pages/about/about.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { Profile } from '../profile/profile';
 
 @Component({
   selector: 'app-about',
@@ -8,11 +9,18 @@ import { Router } from '@angular/router';
   styleUrl: './about.css',
 })
 export class About {
+  @ViewChild("profilePage", { read: ViewContainerRef })
+  container!: ViewContainerRef;
   constructor(public router: Router) {
 
   }
 
   redirectToProfile() {
     this.router.navigate(['profile']);
+  }
+  async dynamicComponentRedirectToProfile() {
+    this.container.clear();
+    const { Profile } = await import('../profile/profile')
+    this.container?.createComponent(Profile);
   }
 }
